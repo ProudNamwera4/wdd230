a0bb5b362d4ecd339b8222fd25f607bf561f1b25
ProudNamwera4 <proudnamwera4@gmail.com>
2023-09-28 08:03:54 +0200

    home page enhancements

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,12 @@ let lastModified = document.querySelector("#lastModified");
 let oLastModif = new Date(document.lastModified);
 
 lastModified.innerHTML = `Last Modified: ${oLastModif}`;
+
+/*Responsive Menu */
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
