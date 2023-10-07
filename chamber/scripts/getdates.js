7130c80395ac6c3fe2278353b8d45682809015ba
ProudNamwera4 <proudnamwera4@gmail.com>
2023-10-07 22:47:34 +0200

    Chamber Website Home Page Build

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,19 @@
+const options = {year : "numeric"};
+document.querySelector("#p").innerHTML = `&copy ${new Date().toLocaleDateString("en-UK", options)} | Proud Namwera | Zimbabwe`;
+
+
+let lastModified = document.querySelector("#lastModified");
+let oLastModif = new Date(document.lastModified);
+
+lastModified.innerHTML = `Last Modified: ${oLastModif}`;
+
+
+
+/*Responsive Menu */
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
