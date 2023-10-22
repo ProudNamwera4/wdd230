1138edbc30eaec47e36a99fadf9521063634d554
ProudNamwera4 <proudnamwera4@gmail.com>
2023-10-22 07:20:46 +0200

    responsive design

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -38,3 +38,5 @@ modeButton.addEventListener("click", () => {
 
 
 
+
+
---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,15 @@
+const temp = document.querySelector("#temperature");
+const button = document.querySelector('#calculate');
+const windSpeed = document.querySelector("#windspeed");
+const windChill = document.querySelector("#windChill");
+
+button.addEventListener('click', ()=> {
+    if(temp.value <= 50 && windSpeed.value>=3.0){
+        let t = (35.74 +0.6215(temp.value))-(35.75(windSpeed.value)**0.16) + (0.4275(temp.value)(windSpeed)**0.16)
+        windChill.textContent = "Wind chill in Fahrenheit: "+ t;
+    }else{
+        windChill.innerHTML = "N/A";
+    }
+})
+
+
