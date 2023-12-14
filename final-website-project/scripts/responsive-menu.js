633ccf643ed569b87bb731380d15e12a30a67d63
ProudNamwera4 <proudnamwera4@gmail.com>
2023-12-14 10:08:29 +0200

    final upload

--- final-website-project/scripts/responsive-menu.js
+++ final-website-project/scripts/responsive-menu.js
@@ -1,9 +1,17 @@
 /*Responsive Menu */
 function myFunction() {
     var x = document.getElementById("myLinks");
+    
     if (x.style.display === "block") {
       x.style.display = "none";
-    } else {
+    } else if (x.matches && x.style.display == "block"){
+      x.style.display = "flex";
+      x.setAttribute("flex-direction","row");
+    }
+    else {
       x.style.display = "block";
     }
   }
+
+  let x = window.matchMedia("max-width:768")
+  
